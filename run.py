5139b0f124635a26305d4ed4471adc39796431cd
Timur <matt_baker@mail.ru>
2023-04-29 11:25:15 +0500

    lesson11_6

--- run.py
+++ run.py
@@ -1,6 +1,20 @@
+from gunicorn.app.base import BaseApplication
 from blog.app import create_app
 
-app = create_app()
+class FlaskGunicornApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
 
-if __name__ == '__main__':
-    app.run()
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+app = create_app()
+gunicorn_app = FlaskGunicornApplication(app, {'bind': '0.0.0.0:8000'})
